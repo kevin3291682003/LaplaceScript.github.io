8c168305e35fd24136ea93d722e4443c16357149
kevin3291682003 <149475035+kevin3291682003@users.noreply.github.com>
2023-12-28 09:56:49 +0800

    更新Beta2自適應大小與複製
    1.更新Beta2自適應大小
    2.點擊卡就能產生語法並複製到剪貼簿

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ function generateCard(cardNumber) {
   var cardPreview = document.getElementById("cardPreview");
   //cardPreview.innerHTML = "img/" + cardNumber + ".png";
   
-  
+  var str = 'hello!';
   if(document.getElementById('big'). checked == true)
-	  cardPreview.innerHTML = "[#p spr_path=\"\" spr_name=\"" + cardNumber + "\" width=\"792\" height=\"1024\"]";
+	  str = "[#p spr_path=\"\" spr_name=\"" + cardNumber + "\" width=\"792\" height=\"1024\"]";
   if(document.getElementById('small'). checked == true)
-	cardPreview.innerHTML = "[#p spr_path=\"\" spr_name=\"" + cardNumber + "\" width=\"396\" height=\"512\"]";
+	str = "[#p spr_path=\"\" spr_name=\"" + cardNumber + "\" width=\"396\" height=\"512\"]";
+
+	cardPreview.innerHTML=str;
+
+const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
 }
 
 
